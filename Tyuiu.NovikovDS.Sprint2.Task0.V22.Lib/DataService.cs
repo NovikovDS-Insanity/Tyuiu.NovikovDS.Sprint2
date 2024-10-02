1febe7d597ed3c2a3f257c71b5435e99129c6e9c
NovikovDS-Insanity <Tyuiu.NovikovDS@outlook.com>
2024-10-02 12:16:28 +0300

    Task0.1

--- Tyuiu.NovikovDS.Sprint2.Task0.V22.Lib/DataService.cs
+++ Tyuiu.NovikovDS.Sprint2.Task0.V22.Lib/DataService.cs
@@ -9,10 +9,10 @@ namespace Tyuiu.NovikovDS.Sprint2.Task0.V22.Lib
             bool[] res = new bool[6];
             res[0] = x == y;
             res[1] = x != y;
-            res[2] = x < y;
+            res[2] = y < x;
             res[3] = x > y;
-            res[4] = x <= y;
-            res[5] = x >= y;
+            res[4] = y <= x;
+            res[5] = y >= x;
 
             return res;
         }
--- Tyuiu.NovikovDS.Sprint2.Task0.V22.Test/DataServiceTest.cs
+++ Tyuiu.NovikovDS.Sprint2.Task0.V22.Test/DataServiceTest.cs
@@ -13,7 +13,7 @@ public class DataServiceTest
         bool[] res = new bool[6];
         res = ds.GetCompareOperations(x, y);
 
-        bool[] wait = new bool[6] { false, true, false, true, false, true };
+        bool[] wait = new bool[6] { false, true, true, true, true, false };
         CollectionAssert.AreEqual(wait, res);
     }
 }
